f81b9851bb5a8f99bd319a5f93d1cf06f7c2ab3c
Jesper Svennevid <jsvennevid@users.noreply.github.com>
2018-05-25 12:45:16 +0200

    Added support for resolve callbacks Added tests for filters Started writing a README

---
+++ README.md
@@ -0,0 +1,73 @@
+
+#### Installation
+
+```
+    npm install --save @microcode/request-schema
+```
+
+## API
+
+new Schema(methods) - Create a new schema, supporting a set of methods.
+
+* methods - What methods to support registration on.
+
+Example:
+```
+    new Schema(['create','read','update','delete']);
+```
+_Creates a new schema supporting CRUD._
+
+schema.on(method, path, [filters], func) - Register a function on a specific method and path in the schema.
+
+* method - What method to register on
+* path - What path to register on
+* filters - Optional list of filters to run before the function
+* func - Function to execute
+
+The function arguments are dynamic (determined when registered), and supports all extracted parameters in the path, along with two special parameters `data` and `context`.
+
+* Normal functions must be named (limitation due to esprima right now), and `context` is used as the context for the function call.
+* Arrow functions requires `context` as a parameter since the context is already overriden.
+
+You can register multiple functions on the same path. If a filter on a function fails (due to e.g. missing authorization), the next function will attempt to run.
+
+Example:
+```
+    schema.on('read', '/objects/:object_id/attributes', new AuthCheckFilter(), (object_id, data, context) => { ... });
+```
+
+schema.run(method, path, data, context) - Run a registered method
+
+Example:
+```
+    schema.run('read', '/objects/1234/attributes', {}, { user_id: 5763 });
+```
+
+### Filters
+
+Filters allow for sharing common functionality to verify and or modify a request. Example use cases could be authentication or request caching.
+
+A filter inherits from the `Filter` class, and implements the method `run`. This method may return a promise.
+
+The data passed into the filter method is a `FilterData` object, and it exposes the following attributes and methods.
+* method - Method type being called
+* data - Data being passed into the function
+* context - Context used in this call
+* resolve(response) - Call to resolve early (without calling function)
+* reject(err) - Call to reject filter (same as throwing an error)
+* on_resolve(func) - Register a callback that runs after the function has successfully executed. Parameters are:
+  * data - Data returned from function
+  * context - Context used in function
+
+Resolve callbacks execute in the order they are registered, and will execute even if a filters resolves early. If a callback throws an exception, the following functions will NOT be executed.
+
+Resolve callbacks may return a promise.
+
+### Paths
+
+* Paths are separated by forward slashes. Example: `/this/is/a/path`
+* Parameters are supported by prefixing a path component with a colon. Example: `/this/is/a/:parameter`. Parameters are made available to the registered function.
+
+### License
+
+This package uses the MIT license. Please read [LICENSE.md] for details.
--- test/schema.js
+++ test/schema.js
@@ -1,4 +1,4 @@
-const { Schema } = require('..');
+const { Schema, Filter } = require('..');
 const assert = require('assert');
 
 describe('Schema', function () {
@@ -86,16 +86,162 @@ describe('Schema', function () {
         await schema.run('read', '/foo', {}, { value: value });
     });
 
-    it('can override data and context parameter names', async function () {
-        const schema = new Schema(['read'], {
-            data: 'foo',
-            context: 'bar'
+    it('runs filter before calling the function', async function () {
+        const schema = new Schema(['read']);
+
+        let filter_executed = false;
+        class TestFilter extends Filter {
+            async run() {
+                filter_executed = true;
+            }
+        }
+
+        let function_executed = false;
+        schema.on('read', '/foo', new TestFilter(), () => {
+            assert.equal(true, filter_executed, "Filter has not executed");
+            function_executed = true;
         });
 
-        schema.on('read', '/test', (foo) => {});
-        schema.on('read', '/test', (bar) => {});
+        await schema.run('read', '/foo', {}, {});
+        assert.equal(true, function_executed, "Function has not executed");
+    });
 
-        assert.throws(() => { schema.on('read', '/test', (data) => {}); }, Error);
-        assert.throws(() => { schema.on('read', '/test', (context) => {}); }, Error);
+    it('does not run function if filter rejects', async function () {
+        const schema = new Schema(['read']);
+
+        class TestFilter extends Filter {
+            async run() {
+                throw new Error("Rejected");
+            }
+        }
+
+        let function_executed = false;
+        schema.on('read', '/foo', new TestFilter(), () => {
+            assert.equal(true, filter_executed, "Filter has not executed");
+            function_executed = true;
+        });
+
+        let err = undefined;
+        await schema.run('read', '/foo', {}, {}).catch(_err => {
+            err = _err;
+        });
+        assert.equal(err.message, "Rejected");
+    });
+
+    it('runs second function if first is rejected', async function () {
+        const schema = new Schema(['read']);
+
+        class TestFilter extends Filter {
+            async run() {
+                throw new Error("Rejected");
+            }
+        }
+
+        let function1_executed = false, function2_executed = false;
+        schema.on('read', '/foo', new TestFilter(), () => {
+            function1_executed = true;
+        });
+        schema.on('read', '/foo', () => {
+            function2_executed = true;
+        });
+
+        await schema.run('read', '/foo', {}, {});
+
+        assert.equal(function1_executed, false, "First function should not execute");
+        assert.equal(function2_executed, true, "Second function should execute");
+    });
+
+    it('can be resolved early by a filter', async function () {
+        const schema = new Schema(['read']);
+
+        class TestFilter extends Filter {
+            async run(filterData) {
+                filterData.resolve("test");
+            }
+        }
+
+        let function_executed = false;
+        schema.on('read', '/foo', new TestFilter(), () => {
+            function_executed = true;
+        });
+
+        const data = await schema.run('read', '/foo', {}, {});
+
+        assert.equal(function_executed, false, "Function should not execute");
+        assert.equal(data, "test");
+    });
+
+    it('can run resolve callbacks registered by a filter', async function () {
+        const schema = new Schema(['read']);
+        const testData = "test";
+        const testContext = {
+            is_context: true
+        };
+
+        let callback_executed = false;
+        class TestFilter extends Filter {
+            async run(filterData) {
+                filterData.on_resolve((data, context) => {
+                    assert.equal(data, testData);
+                    assert.equal(context.is_context, true);
+                    callback_executed = true;
+                });
+            }
+        }
+
+        let function_executed = true;
+        schema.on('read', '/foo', new TestFilter(), () => {
+            function_executed = true;
+            return testData;
+        });
+
+        const data = await schema.run('read', '/foo', {}, testContext);
+
+        assert.equal(function_executed, true, "Function should execute");
+        assert.equal(callback_executed, true, "Callback should execute");
+        assert.equal(data, testData);
+    });
+
+    it('won\'t run resolve callbacks registered by earlier filters', async function () {
+        const schema = new Schema(['read']);
+        const testData = "test";
+        const testContext = {
+            is_context: true
+        };
+
+        class TestFilter1 extends Filter {
+            async run(filterData) {
+                filterData.on_resolve(() => {
+                    throw new Error("Should not execute");
+                });
+            }
+        }
+
+        let callback_executed = false;
+        class TestFilter2 extends Filter {
+            async run(filterData) {
+                filterData.on_resolve((data, context) => {
+                    assert.equal(data, testData);
+                    assert.equal(context.is_context, true);
+                    callback_executed = true;
+                });
+            }
+        }
+
+        schema.on('read', '/foo', new TestFilter1(), () => {
+            throw new Error("This function fails");
+        });
+
+        let function_executed = true;
+        schema.on('read', '/foo', new TestFilter2(), () => {
+            function_executed = true;
+            return testData;
+        });
+
+        const data = await schema.run('read', '/foo', {}, testContext);
+
+        assert.equal(function_executed, true, "Function should execute");
+        assert.equal(callback_executed, true, "Callback should execute");
+        assert.equal(data, testData);
     });
 });
